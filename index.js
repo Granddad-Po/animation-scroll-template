9486cdd5d5c7a0d4637ef9512ff80151bb4e479c
Vitaliy Po <76914279+Granddad-Po@users.noreply.github.com>
2023-11-24 18:05:40 +0400

    feat: add script

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 const boxes = document.querySelectorAll('.box')
 
+const checkBoxes = () => {
+    const trigger = window.innerHeight * 0.8
+
+    for (const box of boxes) {
+        const topOfBox = box.getBoundingClientRect().top
+        if (topOfBox < trigger) {
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show')
+        }
+    }
+}
+
+checkBoxes()
 
 window.addEventListener('scroll', checkBoxes)
